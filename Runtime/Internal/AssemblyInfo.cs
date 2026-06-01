2553ee950c12bf5a5a36a1e0346a757a5a19e7c1
Pieter van de Sluis <pieter.vandesluis@politieacademie.nl>
2026-06-01 16:29:07 +0200

    fix: expose internals to PATween.Tests.Performance assembly
    Performance tests use internal TweenStore/PATweenRunner/Interpolators.Reset;
    add the missing InternalsVisibleTo entry.

--- Runtime/Internal/AssemblyInfo.cs
+++ Runtime/Internal/AssemblyInfo.cs
@@ -3,3 +3,4 @@ using System.Runtime.CompilerServices;
 [assembly: InternalsVisibleTo("PATween.Editor")]
 [assembly: InternalsVisibleTo("PATween.Tests.Editor")]
 [assembly: InternalsVisibleTo("PATween.Tests.Runtime")]
+[assembly: InternalsVisibleTo("PATween.Tests.Performance")]
